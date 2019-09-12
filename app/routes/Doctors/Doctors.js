9effb485ec870162bb5b1916ae5fee5f8e92ab77
hafiz-cormeum <hafiz.zaini@cormeumhealth.com.my>
2019-09-12 16:55:29 +0800

    feat: Implement table searching and lazy loading for pagination

--- app/routes/Doctors/Doctors.js
+++ app/routes/Doctors/Doctors.js
@@ -1,47 +1,192 @@
 import React from "react";
-import { getAll } from "./actions/doctor-actions";
+import { getAllDoctors, updateDoctor } from "./actions/doctor-actions";
 
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { Container, Row, Col, ButtonGroup, Button } from "./../../components";
 
-import paginationFactory, {
-  PaginationProvider,
-  PaginationListStandalone,
-  PaginationTotalStandalone,
-  SizePerPageDropdownStandalone
-} from "react-bootstrap-table2-paginator";
-import filterFactory from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import overlayFactory from "react-bootstrap-table2-overlay";
 
-import { buildCustomTextFilter } from "./filters";
 import { CustomExportCSV } from "./components/CustomExportButton";
 import { CustomSearch } from "./components/CustomSearch";
+
 const sortCaret = order => {
   if (!order) return <i className="fa fa-fw fa-sort text-muted"></i>;
   if (order) return <i className={`fa fa-fw text-muted fa-sort-${order}`}></i>;
 };
 
+const LazyLoad = ({ totalSize, page, sizePerPage, ...rest }) => (
+  <div>
+    <BootstrapTable
+      remote
+      keyField="id"
+      pagination={paginationFactory({
+        page,
+        sizePerPage,
+        showTotal: true,
+        totalSize
+      })}
+      {...rest}
+    />
+  </div>
+);
+
 class Doctors extends React.Component {
-  state = {
-    resource: [],
-    meta: {}
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      resource: [],
+      meta: {},
+      order: "ASC",
+      sortBy: "name",
+      filterById: "",
+      filterByName: "",
+      filterByProviderNumber: "",
+      filterbyLocation: "",
+      sizePerPage: 10,
+      page: 1,
+      isLoading: false,
+      actionModal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
 
   async componentDidMount() {
+    console.log("componentDidMount");
     const {
       data: { resource, meta }
-    } = await getAll();
+    } = await getAllDoctors();
 
     this.setState({ resource, meta });
   }
 
-  handleDeleteRow() {
-    console.log("deleted");
+  getCurrentPage(offset, sizePerPage) {
+    if (offset === 0) return offset + 1;
+    return offset / sizePerPage + 1;
   }
 
+  getPageOffset(page, sizePerPage) {
+    if (page === 1) return page - 1;
+    return (page - 1) * sizePerPage;
+  }
+
+  handleDeleteRow() {
+    console.log("deleted");
+    console.log(
+      "this.resource",
+      this.state.resource,
+      this.state.page,
+      this.state.meta
+    );
+  }
+
+  handlePageChange = async (page, sizePerPage) => {
+    this.setState({ isLoading: true });
+
+    const { order, sortBy } = this.state;
+
+    const offset = this.getPageOffset(page, sizePerPage);
+
+    const params = {
+      offset,
+      order,
+      sortBy,
+      limit: sizePerPage
+    };
+
+    const {
+      data: { resource, meta }
+    } = await getAllDoctors(params);
+
+    this.setState({ resource: resource, meta: meta, page: page });
+    this.setState({ isLoading: false });
+  };
+
+  handleCellEdit = async (collection, cell) => {
+    const { dataField, newValue, rowId } = cell;
+
+    this.setState({ isLoading: true });
+
+    const rowData = collection.find(element => element.id === rowId);
+    rowData[dataField] = newValue;
+
+    const { data } = await updateDoctor(rowData);
+    console.log("Update return");
+    this.setState({ isLoading: false });
+  };
+
+  handleTableSearch = async searchText => {
+    this.setState({ isLoading: true });
+    try {
+      const { sizePerPage, order, sortBy } = this.state;
+      let filterParam = "";
+
+      if (searchText) {
+        filterParam = `
+        (((id CONTAINS ${searchText}) OR (name CONTAINS ${searchText}) OR (providernumber CONTAINS ${searchText}) OR (location CONTAINS ${searchText}) OR (practice CONTAINS ${searchText})))
+        `;
+      }
+      const params = {
+        order,
+        sortBy,
+        limit: sizePerPage,
+        offset: 0,
+        filter: filterParam
+      };
+
+      const {
+        data: { resource, meta }
+      } = await getAllDoctors(params);
+      this.setState({ resource: resource, meta: meta });
+    } catch (error) {
+      console.log("handleTableSearch | ERROR", error);
+    }
+    this.setState({ isLoading: false });
+  };
+
+  handleTableChange = async (type, newState) => {
+    console.log("type", type, newState);
+    if (type === "pagination") {
+      const { page, sizePerPage } = newState;
+      await this.handlePageChange(page, sizePerPage);
+    }
+    if (type === "cellEdit") {
+      const { data, cellEdit } = newState;
+      await this.handleCellEdit(data, cellEdit);
+    }
+    if (type === "search") {
+      const { searchText } = newState;
+      await this.handleTableSearch(searchText);
+    }
+  };
+
+  toggle() {
+    this.setState(prevState => ({
+      actionModal: !prevState.actionModal
+    }));
+  }
+
+  handleTableClick = (e, row, rowIndex) => {
+    console.log("handleTableClick", e, row, rowIndex);
+  };
+
   render() {
-    const { resource, meta } = this.state;
-    console.log("resource", resource);
+    const {
+      resource,
+      meta,
+      order,
+      sortBy,
+      page,
+      sizePerPage,
+      isLoading
+    } = this.state;
+
+    if (resource.length < 1) {
+      return "LOADING...";
+    }
 
     const columns = [
       {
@@ -49,85 +194,50 @@ class Doctors extends React.Component {
         text: "Name",
         sort: true,
         sortCaret,
-        formatter: cell => <span className="text-inverse">{cell}</span>,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by name",
-          getFilter: filter => {
-            this.nameFilter = filter;
-          }
-        })
+        formatter: cell => <span className="text-inverse">{cell}</span>
       },
       {
         dataField: "providernumber",
         text: "Provider Number",
         sort: true,
-        sortCaret,
-        formatter: cell => <span>{cell}</span>,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by provider no",
-          getFilter: filter => {
-            this.providerNumberFilter = filter;
-          }
-        })
+        sortCaret
       },
       {
         dataField: "location",
         text: "Location",
         sort: true,
-        sortCaret,
-        formatter: cell => <span>{cell}</span>,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by location",
-          getFilter: filter => {
-            this.locationFitler = filter;
-          }
-        })
+        sortCaret
       },
       {
         dataField: "practice",
         text: "Practice",
         sort: true,
-        sortCaret,
-        formatter: cell => <span className="text-inverse">{cell}</span>,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by practice",
-          getFilter: filter => {
-            this.practiceFilter = filter;
-          }
-        })
+        sortCaret
       },
       {
         dataField: "verified",
         text: "Verified",
         sort: true,
-        sortCaret,
-        formatter: cell => cell,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by verified",
-          getFilter: filter => {
-            this.verifiedFilter = filter;
-          }
-        })
+        sortCaret
       },
       {
         dataField: "status",
         text: "Status",
         sort: true,
-        sortCaret,
-        formatter: cell => cell,
-        ...buildCustomTextFilter({
-          placeholder: "Filter by status",
-          getFilter: filter => {
-            this.statusFilter = filter;
-          }
-        })
+        sortCaret
+      },
+      {
+        text: "Edit",
+        dataField: "id",
+        formatter: cell => (
+          <i
+            className="fa fa-fw fa-edit"
+            style={{ cursor: "pointer" }}
+            onClick={this.toggle}
+          />
+        )
       }
     ];
-
-    const paginationOption = {
-      custom: true,
-      totalSize: meta.count
-    };
 
     return (
       <ToolkitProvider
@@ -141,7 +251,7 @@ class Doctors extends React.Component {
           <React.Fragment>
             <Container>
               <div className="d-flex justify-content-end align-items-center mb-2">
-                <h6 className="my-0">Doctors</h6>
+                <h5 className="my-0">Doctors</h5>
                 <div className="d-flex ml-auto">
                   <CustomSearch className="mr-2" {...props.searchProps} />
                   <ButtonGroup>
@@ -166,42 +276,52 @@ class Doctors extends React.Component {
                 </div>
               </div>
               {/* START Table */}
-
-              {resource.length > 0 && (
-                <PaginationProvider
-                  pagination={paginationFactory(paginationOption)}
-                >
-                  {({ paginationProps, paginationTableProps }) => (
-                    <div>
-                      <BootstrapTable
-                        classes="table-responsive"
-                        striped
-                        responsive
-                        filter={filterFactory()}
-                        bordered={false}
-                        {...paginationTableProps}
-                        {...props.baseProps}
-                      />
-                      <Row>
-                        <Col lg={{ size: 4, offset: 3 }}>
-                          <Row>
-                            <PaginationListStandalone {...paginationProps} />
-                            {console.log({ ...paginationProps })}
-                            <SizePerPageDropdownStandalone
-                              {...paginationProps}
-                              variation={"dropup"}
-                            />
-                          </Row>
-                        </Col>
-                        <Col lg={4}>
-                          <PaginationTotalStandalone {...paginationProps} />
-                        </Col>
-                      </Row>
-                    </div>
-                  )}
-                </PaginationProvider>
-              )}
-
+              <LazyLoad
+                // classes="table-responsive"
+                bordered={false}
+                responsive
+                striped
+                loading={isLoading}
+                totalSize={meta.count}
+                page={page}
+                sizePerPage={sizePerPage}
+                rowEvents={{ onClick: this.handleTableClick }}
+                onTableChange={this.handleTableChange}
+                overlay={overlayFactory({
+                  styles: {
+                    overlay: base => ({
+                      ...base,
+                      background: "rgba(255, 255, 255, 0.5)"
+                    })
+                  }
+                })}
+                {...props.baseProps}
+              />
+              <Modal
+                isOpen={this.state.actionModal}
+                toggle={this.toggle}
+                className={this.props.className}
+              >
+                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="primary" onClick={this.toggle}>
+                    Do Something
+                  </Button>{" "}
+                  <Button color="secondary" onClick={this.toggle}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </Modal>
               {/* END Table */}
             </Container>
           </React.Fragment>
--- app/routes/Doctors/actions/doctor-actions.js
+++ app/routes/Doctors/actions/doctor-actions.js
@@ -1,17 +1,42 @@
 import axios from "../../../utils/axios";
 import api from "../../../utils/api";
+import qs from "qs";
 
-export const getAll = async () => {
-  const url = api.doctorApiBaseUrl + "/Doctors";
-  console.log("getAll URL", url);
+const defaultParams = {
+  include_count: true,
+  continue: true,
+  order: "ASC",
+  sortBy: "name",
+  filter: "",
+  limit: "10",
+  offset: "0"
+};
 
-  const params = {
+const formatParams = params => {
+  const formattedParams = {
     include_count: true,
     continue: true,
-    order: "",
-    filter: "",
-    limit: "",
-    offset: ""
+    order: `${params.sortBy} ${params.order}`,
+    offset: `${params.offset}/resource`,
+    filter: params.filter,
+    limit: params.limit
   };
+
+  return formattedParams;
+};
+
+export const getAllDoctors = async (params = defaultParams) => {
+  console.log("API | GET | /Doctors");
+  const formattedParams = formatParams(params);
+  const url = `${api.doctorApiBaseUrl}/Doctors?${qs.stringify(
+    formattedParams
+  )}`;
   return axios.get(url, params);
+};
+
+export const updateDoctor = async data => {
+  console.log("API | PATCH | /Doctors");
+  const url = `${api.doctorApiBaseUrl}/Doctors`;
+  const updatedData = { resource: [data] };
+  return axios.patch(url, updatedData);
 };
